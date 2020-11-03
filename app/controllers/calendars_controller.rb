663a9f21acb14db22365110abdc8bdc192e6a3de
NAGA-87 <naga.2000.chr5@gmail.com>
2020-11-03 12:17:47 +0900

    calendars_controller.rbデバッグ用コード他不要記述削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -10,7 +10,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -38,14 +37,11 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wday_num = Date.today.wday
-      #もしもwday_numが7以上であれば、7を引く
-      # if wday_num >= 7
+
         wday_num -= 7 - x
-      # end
+
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :wdays => wdays[wday_num], :plans => today_plans}
-      # binding.pry if x == 6
       @week_days.push(days)
-      # binding.pry
     end
 
   end
